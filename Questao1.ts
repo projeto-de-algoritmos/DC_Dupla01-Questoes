b12b650ab3728cebb126ef4555988d5c5a6c9c82
Matheus Pimentel <matheusp.eng@gmail.com>
2023-01-01 23:10:22 -0300

    :star: adicionando mais estrutura ao código
    Co-authored-by: Giulia Lobo Barros <giulialobo89@gmail.com>

--- Questao1.ts
+++ Questao1.ts
@@ -10,24 +10,25 @@ class TreeNode {
     }
 }
 
+// Função ajudante para organização da árvore binária
+function helper(nums: number[], left:number, right:number): TreeNode | null {
+    
+    return null;
+}
+
 function sortedArrayToBST(nums: number[]): TreeNode | null {
     // Caso o array submetido seja nulo, retornar nulo
     if (nums.length === 0) {
         return null;
     }
-    
-    // Inicialiação da árvore binária de pesquisa
-    let Tree: TreeNode = new TreeNode();
 
-    nums.map((num, index) => {
-        // Para o primeiro elemento da lista
-        if(index === 0) {
-            Tree.val = num;
-            return;
-        }
-        // Balancear a árvore para cada novo número
-    });
+    // Pegar o índice do nó raiz da árvore, já que o problema trata apenas soluções de array ordenado
+    const rootIndex: number = Math.floor(nums.length / 2);
+    // Inicialiação da árvore binária de pesquisa
+    let Tree: TreeNode = new TreeNode(nums[rootIndex]);
     
+
+
     return Tree;
 };
 
