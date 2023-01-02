acaf5379846dd1406741caf705e00065e7c112e7
Giulia Lobo <42353909+Giuulob89@users.noreply.github.com>
2023-01-02 19:59:02 -0300

    Adição da solução final do problema.
    Co-authored-by: Matheus Pimentel Leal <matheusp.eng@gmail.com>

--- Questao1.ts
+++ Questao1.ts
@@ -4,16 +4,31 @@ class TreeNode {
     left: TreeNode | null
     right: TreeNode | null
     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-        this.val = (val===undefined ? 0 : val)
-        this.left = (left===undefined ? null : left)
-        this.right = (right===undefined ? null : right)
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
     }
 }
 
 // Função ajudante para organização da árvore binária
-function helper(nums: number[], left:number, right:number): TreeNode | null {
-    
-    return null;
+function helper(nums: number[], left: number, right: number): TreeNode | null {
+    // Retornar nulo como forma de captura de erros
+    if (left > right) {
+        return null;
+    }
+
+    // Encontrar o meio do array e criar um novo nó para ele (raiz da árvore)
+    let meio: number = Math.floor((left + right) / 2);
+    let raizArvore: TreeNode = new TreeNode(nums[meio]);
+
+    // Dividir o array na parte esquerda (do extremo esquerdo até antes do meio)
+    raizArvore.left = helper(nums, left, meio - 1);
+
+    // Dividir o array na parte esquerda (do extremo esquerdo até antes do meio)
+    raizArvore.right = helper(nums, meio + 1, right);
+
+    // Retornar a árvore binária de pesquisa
+    return raizArvore;
 }
 
 function sortedArrayToBST(nums: number[]): TreeNode | null {
@@ -22,14 +37,9 @@ function sortedArrayToBST(nums: number[]): TreeNode | null {
         return null;
     }
 
-    // Pegar o índice do nó raiz da árvore, já que o problema trata apenas soluções de array ordenado
-    const rootIndex: number = Math.floor(nums.length / 2);
-    // Inicialiação da árvore binária de pesquisa
-    let Tree: TreeNode = new TreeNode(nums[rootIndex]);
-    
-
+    const Tree: TreeNode | null = helper(nums, 0, nums.length - 1);
 
     return Tree;
 };
 
-console.log(sortedArrayToBST([-10,-5,0,5,9]));
+console.log(sortedArrayToBST([-10, -3, 0, 5, 9]));
